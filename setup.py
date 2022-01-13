91e83600aef3965fc93d2d1b9566acf40b88b151
Yannik Marchand <ymarchand@me.com>
2022-01-13 18:40:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@
 import setuptools
 
 description = \
-	"Asynchronous netlink library"
+	"Asynchronous netlink library for Python"
 
-long_description = description
+long_description = \
+	"This is an asynchronous netlink library for Python, " \
+	"including basic support for nl80211."
 
 setuptools.setup(
 	name = "python-netlink",
-	version = "0.0.3",
+	version = "0.0.4",
 	description = description,
 	long_description = long_description,
 	author = "Yannik Marchand",
 	author_email = "ymarchand@me.com",
-	url = "https://github.com/kinnay/python-netlink",
+	url = "https://github.com/kinnay/netlink",
 	license = "GPLv3",
 	platforms = ["Linux"],
 	packages = ["netlink"],
