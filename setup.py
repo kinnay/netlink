e5e41ebeca78ffd9bbbf5db11aabf5e583a10e1e
Yannik Marchand <ymarchand@me.com>
2025-09-22 15:28:04 +0200

    Release 0.0.10

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = \
 
 setuptools.setup(
 	name = "python-netlink",
-	version = "0.0.9",
+	version = "0.0.10",
 	description = description,
 	long_description = long_description,
 	author = "Yannik Marchand",
